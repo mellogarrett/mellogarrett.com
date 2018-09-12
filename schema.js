8116520788aae31bd78e99d21b390a6901f5c0d0
mellogarrett <garrett6346@gmail.com>
2018-09-12 08:40:22 -0400

    install npm packages. import all types and resolvers in schema.js

--- schema.js
+++ schema.js
@@ -1,12 +1,44 @@
 const { gql } = require("apollo-server-express");
+const merge = require("lodash.merge");
 
-const baseSchema = gql`
+// types
+const achievementType = require("./types/achievement");
+const educationType = require("./types/education");
+const jobType = require("./types/job");
+const linkType = require("./types/link");
+const resumeType = require("./types/resume");
+const skillType = require("./types/skill");
+// resolvers
+const achievementResolver = require("./resolvers/achievement");
+const educationResolver = require("./resolvers/education");
+const jobResolver = require("./resolvers/job");
+const linkResolver = require("./resolvers/link");
+const resumeResolver = require("./resolvers/resume");
+const skillResolver = require("./resolvers/skill");
+
+const baseType = gql`
   type Query {
     hello: String
   }
 `;
 
 module.exports = {
-  typeDefs,
-  resolvers
+  typeDefs: [
+    baseType,
+    achievementType,
+    educationType,
+    jobType,
+    linkType,
+    resumeType,
+    skillType
+  ],
+  resolvers: merge(
+    {},
+    achievementResolver,
+    educationResolver,
+    jobResolver,
+    linkResolver,
+    resumeResolver,
+    skillResolver
+  )
 };
